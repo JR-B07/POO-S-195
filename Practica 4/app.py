09fbebcf58fca38b90e3bba875e5500fb9a5549f
JR-B07 <121039413@upq.edu.mx>
2024-05-31 21:52:09 -0600

    Practica 4 parte 2
    importamos la biblioteca de flask para poder trabajar con MYSQL, declaramos las variables que nos permitirán la conexion con la BD y creamos una ruta que nos confirmará dicha conexión

--- Practica 4/app.py
+++ Practica 4/app.py
@@ -1,12 +1,30 @@
-from flask import Flask, request
+from flask import Flask, request,jsonify
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '12345'
+app.config['MYSQL_DB'] = 'dbflask'
+mysql = MySQL(app)
+
 
 #RUTA SIMPLE
 @app.route('/')
 def principal():
     return 'hola mundo Flask'
 
+#RUTA SIMPLE
+@app.route('/PruebaConexion')
+def PruebaConexion():
+    try:
+        cursor= mysql.connection.cursor()
+        cursor.execute('SELECT 1')
+        datos = cursor.fetchone()
+        return jsonify({'status': 'Conexion Exitosa', 'data':datos})
+    except Exception as ex:
+        return jsonify({'status': 'Error de Conexion', 'mensaje': str(ex)})
+    
 #RUTA DOBLE
 @app.route('/usuario')
 @app.route('/saludar')
@@ -26,6 +44,8 @@ def formulario():
     elif request.method == 'POST':
         return 'POST si es seguro para passwords'
     
+
+
 #Manejo de excepciones para rutas     
 @app.errorhandler(404)
 def paginanotfound(e):
